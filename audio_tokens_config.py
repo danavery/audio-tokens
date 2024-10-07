90cf2fe9009a42bdddf270c78f44e8602914c95b
Dan Avery <dan.github@danavery.com>
2024-10-07 14:22:49 -0700

    config for BERT run

--- audio_tokens_config.py
+++ audio_tokens_config.py
@@ -16,25 +16,25 @@ class AudioTokensConfig:
     csv_index_files: List[str] = field(
         default_factory=lambda: [
             "metadata/balanced_train_segments.csv",
-            # "metadata/unbalanced_train_segments.csv",
+            "metadata/unbalanced_train_segments.csv",
         ]
     )
     ontology_json_file: str = "metadata/ontology.json"
-    dataset_ratio: float = 1  # portion of all ytids to use
+    dataset_ratio: float = 0.1  # portion of all ytids to use
     validation_ratio: float = 0.1  # portion of dataset to use as validation set
 
     # AudiosetMetadataProcessor and SpectrogramProcessor
-    split_file: str = "output/bal_train_val_data_split.json"
+    split_file: str = "output/bal_train_data_split.json"
 
     # SpectrogramProcessor
     audio_source_path: str = "/media/davery/audioset"
-    audio_source_sets: List[str] = field(default_factory=lambda: ["bal_train"])
-    dest_spec_path: Path = Path("spectrograms/")
+    audio_source_sets: List[str] = field(default_factory=lambda: ["bal_train", "unbal_train"])
+    dest_spec_path: Path = Path("spectrograms_big/")
     common_sr: int = 22050
-    normalize: bool = True
+    normalize: bool = False
     n_mels: int = 64
     n_fft: int = 512
-    hop_length: int = 256
+    hop_length: int = 128
     n_segments: int = 0
     spectrogram_batch_size: int = 5000
 
@@ -50,29 +50,29 @@ class AudioTokensConfig:
 
     # ClusterCreator and SpecTokenizer
     centroids_path: Path = Path("output/centroids.npy")
-    source_spec_path: Path = Path("spectrograms/")
+    source_spec_path: Path = Path("spectrograms_big/")
 
     # SpecTokenizer config
     dest_tokenized_path: str = "tokenized_audio/"
     tokenizer_batch_size: int = 10000
 
     # ModelTrainer
-    use_wandb: bool = False
+    use_wandb: bool = True
     tokenized_train_dir: str = "tokenized_audio/train/"
     tokenized_val_dir: str = "tokenized_audio/validation/"
-    model_type: str = "lstm"
+    model_type: str = "bert"
     num_layers: int = 1
     epochs: int = 100
     hidden_size: int = 768
-    training_batch_size: int = 63
+    training_batch_size: int = 32
     num_workers: int = 8
-    learning_rate: float = 1e-4
+    learning_rate: float = 1e-5
     num_classes: int = 543
     prediction_threshold: float = 0.2
-    lstm_embed_dim: int = 64
-    lstm_hidden_dim: int = 256
+    lstm_embed_dim: int = 256
+    lstm_hidden_dim: int = 512
     dropout: float = 0.0
-    use_precomputed_embeddings = True  # Use True for RawSTFTDataset
+    use_precomputed_embeddings = False  # Use True for RawSTFTDataset
 
     # DataLoaderCreator
-    dataset_type: str = "RawSTFTDataset"
+    dataset_type: str = "TokenizedSpecDataset"
